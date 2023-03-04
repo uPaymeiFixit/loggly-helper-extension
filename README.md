6a6bf904079afbf74bfc2ac66d8f32815185de11
uPaymeiFixit <josh@gibbs.tk>
2023-03-03 18:55:47 -0800

    🚸 create extension

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # loggly-helper-extension
-A chrome extension which parses stringified JSON, most commonly found in the raw log.
 
-Loggly has a limited number of indexes, so users find themselves sending stringified JSON, which is difficult to read. This extension, which began as a bookmarklet, will find any pars-able JSON on the page and format it. 
+An extension which parses stringified JSON, most commonly found in the raw log.
+
+Loggly has a limited number of indexes, so users find themselves sending stringified JSON, which is difficult to read. This extension, which began as a bookmarklet, will find any pars-able JSON on the page and format it.
 
 ![2023-03-03 17 26 31](https://user-images.githubusercontent.com/1683528/222868210-7806d390-61e0-44c6-864a-86b70f3c1641.gif)
---
+++ main.js
@@ -0,0 +1,56 @@
+const wrapValue = (value) => {
+  let type = "str";
+  if (typeof value === "number" || typeof value === "boolean") type = "num";
+  return `<span lg-term-highlight-node-value="::node" lg-view-code-marker="::node" uib-tooltip="Add to Search" tooltip-placement="top" tooltip-trigger="focus" tooltip-append-to-body="false" tooltip-enable="isRowTooltipOpen" class="node-value node-hover lg-pretty-json"><span class="json-${type}">${value}</span></span>`;
+};
+const wrapKey = (key) => `<span style="color:#AAAAAA;">${key}</span>`;
+
+// Apply color, new lines, and spacing to a parsed object
+function formatObject(obj, depth = 0) {
+  let o = "";
+  if (typeof obj === "object") {
+    for (const key in obj) {
+      o += `\n${"  ".repeat(depth)}${wrapKey(key)}: ${formatObject(obj[key], depth + 1)}`;
+    }
+    return o;
+  }
+  return wrapValue(obj);
+}
+
+// Recursively parse an object whose values may also be stringified json
+function deepParse(obj) {
+  try {
+    return JSON.parse(obj, (key, value) => deepParse(value));
+  } catch (error) {}
+  return obj;
+}
+
+function scanAndFormat() {
+  // Pause the observer while we update the DOM so we don't trigger it
+  observer.disconnect();
+
+  // Format `raw message` contents
+  document.querySelectorAll("div[lg-lazy-show='rawMessageExpanded']").forEach((element) => {
+    const p = deepParse(element.innerHTML.replaceAll('<span class="highlight">', "<span class='highlight'>"));
+    element.innerHTML = formatObject(p);
+  });
+
+  // Format all other string values
+  document.querySelectorAll(".json-str").forEach((element) => {
+    const safe_element = element.innerHTML.replaceAll('<span class="highlight">', "<span class='highlight'>");
+
+    try {
+      const p = deepParse(safe_element);
+      if (p === safe_element) return;
+
+      element.innerHTML = formatObject(p);
+    } catch (error) {}
+  });
+
+  // Resume the observer
+  observer.observe(document.body, { attributes: true, childList: true, subtree: true });
+}
+
+// Create an observer to detect changes to the DOM (unfolding, new logs, etc)
+const observer = new MutationObserver(scanAndFormat);
+observer.observe(document.body, { attributes: true, childList: true, subtree: true });
